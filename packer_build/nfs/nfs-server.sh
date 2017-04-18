39590baaf5e128b2c891e85fcd010e3bb4742249
tamutamu <tamu.0.0.tamu@gmail.com>
2017-04-18 17:36:33 +0900

    Add NFS.

---
+++ packer_build/nfs/nfs-server.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -euo pipefail
+
+echo "Install NFS Server." 
+
+CURDIR=$(cd $(dirname $0); pwd)
+. ./var.conf
+
+### Install
+yum -y install nfs-utils nfs-utils-lib
+
+mkdir /var/exports
+echo '/var/exports  *(rw,all_squash,fsid=0,crossmnt)' >> /etc/exports
+exportfs -r
+exportfs -v
