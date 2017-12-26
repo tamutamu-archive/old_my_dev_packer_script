407f8bc04db7dce71388e79c3661256d74860a4e
tamutamu <tamu.0.0.tamu@gmail.com>
2017-12-26 09:13:14 +0900

    Fix mod_python

--- packer_build/python/mod_python.sh
+++ packer_build/python/mod_python.sh
@@ -3,12 +3,11 @@
 
 pushd /usr/local/src
 
-wget http://dist.modpython.org/dist/mod_python-3.5.0.tgz
-tar zxvf mod_python-3.5.0.tgz
+git clone https://github.com/grisha/mod_python.git
 
-sed -i -e "s#git describe --always#git describe --always > /dev/null 2>&1#" /usr/local/src/mod_python-3.5.0/dist/version.sh
+pushd mod_python
+###sed -i -e "s#git describe --always#git describe --always > /dev/null 2>&1#" /usr/local/src/mod_python-3.5.0/dist/version.sh
 
-pushd mod_python-3.5.0/
 ./configure --with-python=/usr/bin/python2.7 --with-apxs=/bin/apxs
 make && make install
 popd
